1f5f472233ee678d613c7f7512407c19432f714e
Maxence <max@23.tf>
2019-02-24 19:03:53 -0500

    Add TaskList with redis, and conversion wrapper

--- calibre_wrapper/calibredb.py
+++ calibre_wrapper/calibredb.py
@@ -2,15 +2,19 @@ import subprocess
 from sqlalchemy import create_engine, Table, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import select
+from threading import Thread
 import os
-import tempfile
-
+import uuid
 
 class CalibreDBW:
-    def __init__(self, calibre_lib_dir):
-        self._calibre_lib_dir = calibre_lib_dir
+    def __init__(self, config, redis_db):
+        self._task_list_name = 'CALIBRE_WEBUI_TASKS_LIST'
+        self._redis_db = redis_db
+        self._config = config
+        self._calibre_lib_dir = self._config['CALIBRE_LIBRARY_PATH']
         self._calibre_db = os.path.join(self._calibre_lib_dir, 'metadata.db')
-        self.db_ng = create_engine('sqlite:///%s' % self._calibre_db)
+        self._db_ng = create_engine('sqlite:///%s' % self._calibre_db)
+        self.clear_tasks()
 
     def add_book(self, file_path):
         return subprocess.call(['calibredb', 'add', '-d', file_path,
@@ -29,9 +33,64 @@ class CalibreDBW:
         return subprocess.call(['calibredb', 'remove', '--permanent',
             '--library-path', self._calibre_lib_dir, str(book_id)])
 
+    def list_tasks(self):
+        tasks_list = self._redis_db.lrange(self._task_list_name, 0, -1)
+        return [self._redis_db.hgetall(task) for task in tasks_list]
+
+    def clear_tasks(self):
+        self._redis_db.delete(self._task_list_name)
+
+
+    def tasks_count(self):
+        tasks = self.list_tasks()
+        return {
+            'CANCELED':
+                sum(t['status'] == 'CANCELED' for t in tasks),
+            'RUNNING':
+                sum(t['status'] == 'RUNNING' for t in tasks),
+            'COMPLETED':
+                sum(t['status'] == 'COMPLETED' for t in tasks),
+            }
+    def create_redis_task(self, message, status):
+        task_id = uuid.uuid4().hex
+        task = {
+                'status': status,
+                'message': message
+                }
+        self._redis_db.hmset(task_id, task)
+        self._redis_db.lpush(self._task_list_name, task_id)
+        return task_id
+
+    def update_task_status(self, task_id, status):
+        self._redis_db.hmset(task_id, {'status': status})
+
+    def threaded(fn):
+        def wrapper(*args, **kwargs):
+            thread = Thread(target=fn, args=args, kwargs=kwargs)
+            thread.start()
+            return thread
+        return wrapper
+
+    @threaded
+    def convert_book(self, book_id, format_from, format_to):
+        task_id = self.create_redis_task('Convert book « %s » from %s to %s'
+                %  (self.get_book_title(book_id), format_from, format_to),
+                'RUNNING')
+        fpath, fname = self.get_book_file(book_id, format_from)
+        tmp_dir = self._config['CALIBRE_TEMP_DIR']
+        tmp_file = os.path.join(tmp_dir, 'calibre_temp_%s.%s' % (book_id,
+            format_to.lower()))
+        fullpath = os.path.join(fpath, fname)
+        if subprocess.call(['ebook-convert', fullpath, tmp_file]) == 0:
+            self.add_format(book_id, tmp_file)
+            os.remove(tmp_file)
+            self.update_task_status(task_id, 'COMPLETED')
+        else:
+            self.update_task_status(task_id, 'CANCELED')
+
     def search_books_tags(self, search):
-        with self.db_ng.connect() as con:
-            meta = MetaData(self.db_ng)
+        with self._db_ng.connect() as con:
+            meta = MetaData(self._db_ng)
             books = Table('books', meta, autoload=True)
             books_authors_link = Table('books_authors_link',
                     meta, autoload=True)
@@ -56,8 +115,8 @@ class CalibreDBW:
             return con.execute(stm).fetchall()
 
     def search_books_series(self, search):
-        with self.db_ng.connect() as con:
-            meta = MetaData(self.db_ng)
+        with self._db_ng.connect() as con:
+            meta = MetaData(self._db_ng)
             books = Table('books', meta, autoload=True)
             books_authors_link = Table('books_authors_link',
                     meta, autoload=True)
@@ -82,8 +141,8 @@ class CalibreDBW:
             return con.execute(stm).fetchall()
 
     def search_books_authors(self, search):
-        with self.db_ng.connect() as con:
-            meta = MetaData(self.db_ng)
+        with self._db_ng.connect() as con:
+            meta = MetaData(self._db_ng)
             books = Table('books', meta, autoload=True)
             books_authors_link = Table('books_authors_link',
                     meta, autoload=True)
@@ -109,8 +168,8 @@ class CalibreDBW:
             elif attribute == 'tags':
                 return self.search_books_tags(search)
         else:
-            with self.db_ng.connect() as con:
-                meta = MetaData(self.db_ng)
+            with self._db_ng.connect() as con:
+                meta = MetaData(self._db_ng)
                 books = Table('books', meta, autoload=True)
                 books_authors_link = Table('books_authors_link',
                         meta, autoload=True)
@@ -140,11 +199,11 @@ class CalibreDBW:
 
     def list_tags(self):
         tags_list = []
-        meta = MetaData(self.db_ng)
+        meta = MetaData(self._db_ng)
         tags = Table('tags', meta, autoload=True)
         books_tags_link = Table('books_tags_link', meta, autoload=True)
 
-        with self.db_ng.connect() as con:
+        with self._db_ng.connect() as con:
             stm = select([tags]).order_by(tags.c.name)
             for tag in con.execute(stm).fetchall():
                 stm = select([books_tags_link])\
@@ -155,11 +214,11 @@ class CalibreDBW:
 
     def list_series(self):
         series_list = []
-        meta = MetaData(self.db_ng)
+        meta = MetaData(self._db_ng)
         series = Table('series', meta, autoload=True)
         books_series_link = Table('books_series_link', meta, autoload=True)
 
-        with self.db_ng.connect() as con:
+        with self._db_ng.connect() as con:
             stm = select([series]).order_by(series.c.name)
             for serie in con.execute(stm).fetchall():
                 stm = select([books_series_link])\
@@ -170,11 +229,11 @@ class CalibreDBW:
 
     def list_authors(self):
         authors_list = []
-        meta = MetaData(self.db_ng)
+        meta = MetaData(self._db_ng)
         authors = Table('authors', meta, autoload=True)
         books_authors_link = Table('books_authors_link', meta, autoload=True)
 
-        with self.db_ng.connect() as con:
+        with self._db_ng.connect() as con:
             stm = select([authors]).order_by(authors.c.name)
             for author in con.execute(stm).fetchall():
                 stm = select([books_authors_link])\
@@ -185,8 +244,8 @@ class CalibreDBW:
         return authors_list
 
     def get_book(self, book_id):
-        with self.db_ng.connect() as con:
-            meta = MetaData(self.db_ng)
+        with self._db_ng.connect() as con:
+            meta = MetaData(self._db_ng)
             books = Table('Books', meta, autoload=True)
             books_authors_link = Table('books_authors_link',
                     meta, autoload=True)
@@ -208,8 +267,8 @@ class CalibreDBW:
 
     def get_book_formats(self, book_id):
         formats = []
-        with self.db_ng.connect() as con:
-            meta = MetaData(self.db_ng)
+        with self._db_ng.connect() as con:
+            meta = MetaData(self._db_ng)
             data = Table('Data', meta, autoload=True)
             stm = select([data]).where(data.c.book == book_id)
             for book_format in con.execute(stm).fetchall():
@@ -218,10 +277,17 @@ class CalibreDBW:
                         book_format.uncompressed_size / (1024*1024))})
             return formats
 
+    def get_book_title(self, book_id):
+        with self._db_ng.connect() as con:
+            meta = MetaData(self._db_ng)
+            books = Table('Books', meta, autoload=True)
+            stm = select([books.c.title]).where(books.c.id == book_id)
+            return con.execute(stm).first().title
+
     def get_book_file(self, book_id, book_format):
         book_path = self.get_book(book_id).path
-        with self.db_ng.connect() as con:
-            meta = MetaData(self.db_ng)
+        with self._db_ng.connect() as con:
+            meta = MetaData(self._db_ng)
             data = Table('Data', meta, autoload=True)
             stm = select([data]).where(data.c.book == book_id)
             for fmt in con.execute(stm).fetchall():
