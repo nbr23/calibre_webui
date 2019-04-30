46519c02f4d61d56db8a3b73bf7f9bf89a44b4ea
Maxence <max@23.tf>
2019-04-29 21:47:14 -0400

    Clean up endpoints and segregate device endpoints

--- calibre_webui/routes.py
+++ calibre_webui/routes.py
@@ -19,10 +19,47 @@ def flash_debug(message):
 def flash_success(message):
     return flash(message, app.FLASH['success'])
 
+# API Endpoints
+
+@app.route('/api/books/list')
+def get_books():
+    page = int(request.args.get('page')) if 'page' in request.args else 1
+    search = request.args.get("search").strip().lower() \
+            if 'search' in request.args else None
+    scope = request.args.get('search_scope').strip() \
+            if 'search_scope' in request.args else None
+    books = app.calibredb_wrap.search_books(search.lower() if search else None,
+            scope.lower() if scope else None,
+            page=page)
+    if not books:
+        return jsonify([])
+    return jsonify(app.calibredb_wrap.resultproxy_to_dict(books))
+
+@app.route('/api/books/<int:book_id>/formats')
+def get_book_formats(book_id):
+    return jsonify(app.calibredb_wrap.get_book_formats(book_id))
+
+@app.route('/api/tasks/list')
+def get_tasks_list():
+    tasks = app.calibredb_wrap.list_tasks()
+    return jsonify(tasks)
+
+@app.route('/api/tasks/count')
+def get_tasks_count():
+    return jsonify(app.calibredb_wrap.tasks_count())
+
+@app.route('/api/tasks/clear')
+def clear_tasks():
+    app.calibredb_wrap.clear_tasks()
+    return jsonify({})
+
+# File serving
 @app.endpoint('static')
 def static(filename):
     static_url = app.config['STATIC_URL']
     return redirect(urljoin(static_url, filename))
+
+## Views ##
 
 @app.route('/', methods=['GET'])
 def index():
@@ -33,15 +70,15 @@ def index():
     return render_template('index.html', search=search,
             scope=scope, title='My Books')
 
+# Device Management
+@app.route('/devices/list')
+def device_list():
+    devices = app.database.list_devices()
+    url = urljoin(request.host_url, url_for('device_register'))
+    return render_template('device_list.html', deviceslist=devices,
+            url=url, title='Devices')
 
-@app.route('/feed/')
-def device_register():
-    uid = app.database.get_new_uid()
-    activate_url = '%sdevice/edit/%s' % (request.host_url, uid)
-    return render_template('register_device.html', activate_url=activate_url,
-            device_id=uid, title='New Device')
-
-@app.route('/device/save', methods=['POST'])
+@app.route('/devices/save', methods=['POST'])
 def device_save():
     if 'device_id' in request.form and \
             'device_name' in request.form and \
@@ -59,7 +96,7 @@ def device_save():
         flash_error('Couldn\'t save device')
     return redirect(url_for("device_list"))
 
-@app.route('/device/edit/<device_id>')
+@app.route('/devices/edit/<device_id>')
 def device_edit(device_id):
     device = app.database.get_device(uid=device_id)
     if not device:
@@ -67,7 +104,7 @@ def device_edit(device_id):
     return render_template('activate_device.html', device=device,
             title='Activate Device')
 
-@app.route('/device/delete/<device_id>')
+@app.route('/devices/delete/<device_id>')
 def device_delete(device_id):
     device = app.database.get_device(uid=device_id)
     if device:
@@ -77,16 +114,16 @@ def device_delete(device_id):
         flash_error('Device not found')
     return redirect(url_for("device_list"))
 
-@app.route('/device/list')
-def device_list():
-    devices = app.database.list_devices()
-    url = urljoin(request.host_url, url_for('device_register'))
-    return render_template('device_list.html', deviceslist=devices,
-            url=url, title='Devices')
+# Book feed for reading devices
+@app.route('/feeds/')
+def device_register():
+    uid = app.database.get_new_uid()
+    activate_url = '%sdevices/edit/%s' % (request.host_url, uid)
+    return render_template('register_device.html', activate_url=activate_url,
+            device_id=uid, title='New Device')
 
-
-@app.route('/feed/<device_id>')
-@app.route('/feed/<device_id>/<int:page>')
+@app.route('/feeds/<device_id>')
+@app.route('/feeds/<device_id>/<int:page>')
 def device_feed(device_id, page=1):
     device = app.database.get_device(device_id)
     if not device:
@@ -96,39 +133,26 @@ def device_feed(device_id, page=1):
     return render_template('feed.html', books=books, title=device.name,
             book_format=book_format, page=page, device_id=device_id)
 
-@app.route('/api/books')
-def get_books():
-    page = int(request.args.get('page')) if 'page' in request.args else 1
-    search = request.args.get("search").strip().lower() \
-            if 'search' in request.args else None
-    scope = request.args.get('search_scope').strip() \
-            if 'search_scope' in request.args else None
-    books = app.calibredb_wrap.search_books(search.lower() if search else None,
-            scope.lower() if scope else None,
-            page=page)
-    if not books:
-        return jsonify([])
-    return jsonify(app.calibredb_wrap.resultproxy_to_dict(books))
+@app.route('/feeds/<device_id>/books/<int:book_id>/file/<book_format>/')
+def device_feed_download_book_file(device_id, book_id, book_format):
+    device = app.database.get_device(device_id)
+    if not device:
+        return redirect(url_for("device_register"))
+    return download_book_file(book_id, book_format)
 
+@app.route('/feeds/<device_id>/books/<int:book_id>/cover')
+def device_feed_get_cover(device_id, book_id):
+    device = app.database.get_device(device_id)
+    if not device:
+        return redirect(url_for("device_register"))
+    return get_cover(book_id)
+
+# List views
 @app.route('/tasks')
 def list_tasks():
     tasks = app.calibredb_wrap.list_tasks()
     return render_template('tasklist.html', tasklist=tasks,
             title='Tasks list')
-
-@app.route('/api/tasks')
-def get_tasks_list():
-    tasks = app.calibredb_wrap.list_tasks()
-    return jsonify(tasks)
-
-@app.route('/api/tasks/count')
-def get_tasks_count():
-    return jsonify(app.calibredb_wrap.tasks_count())
-
-@app.route('/api/tasks/clear')
-def clear_tasks():
-    app.calibredb_wrap.clear_tasks()
-    return jsonify({})
 
 @app.route('/authors')
 def list_authors():
@@ -148,16 +172,8 @@ def list_series():
     return render_template('list.html', itemlist=series,
             title='Series list', scope='series')
 
-@app.route('/cover/<int:book_id>')
-def get_cover(book_id):
-    book = app.calibredb_wrap.get_book(book_id)
-    if book and book.has_cover:
-        return send_from_directory(os.path.join(
-            app.config['CALIBRE_LIBRARY_PATH'], book.path),
-            'cover.jpg')
-    return redirect('/static/img/default_cover.jpg')
-
-@app.route('/book/<int:book_id>/edit', methods=['GET', 'POST'])
+# Books
+@app.route('/books/<int:book_id>/edit', methods=['GET', 'POST'])
 def book_edit(book_id):
     book, formats = app.calibredb_wrap.get_book_details(book_id)
     if not book:
@@ -170,7 +186,7 @@ def book_edit(book_id):
             formats_to=app.config['CALIBRE_EXT_CONV'],
             preferred=app.config['FORMAT_PREFERRED'])
 
-@app.route('/book/<int:book_id>/save', methods=['POST'])
+@app.route('/books/<int:book_id>/save', methods=['POST'])
 def book_save(book_id):
     metadata = {}
     book, formats = app.calibredb_wrap.get_book_details(book_id)
@@ -197,18 +213,7 @@ def book_save(book_id):
         flash_error('Error updating metadata')
     return redirect(url_for("book_edit", book_id=book_id))
 
-
-@app.route('/book/<int:book_id>/file/<book_format>/')
-def download_book_file(book_id, book_format):
-    fpath, fname = app.calibredb_wrap.get_book_file(book_id,
-            book_format.upper())
-    return send_from_directory(fpath, fname, conditional=True, attachment_filename=fname, as_attachment=True)
-
-@app.route('/book/<int:book_id>/formats')
-def get_book_formats(book_id):
-    return jsonify(app.calibredb_wrap.get_book_formats(book_id))
-
-@app.route('/book/<int:book_id>/formats/<book_format>/delete')
+@app.route('/books/<int:book_id>/formats/<book_format>/delete')
 def delete_book_format(book_id, book_format):
     if app.calibredb_wrap.remove_format(book_id, book_format) == 0:
         flash_success('%s deleted!' % book_format)
@@ -216,7 +221,7 @@ def delete_book_format(book_id, book_format):
         flash_error('Could not delete %s' % book_format)
     return redirect(url_for("book_edit", book_id=book_id))
 
-@app.route('/book/<int:book_id>/delete')
+@app.route('/books/<int:book_id>/delete')
 def delete_book(book_id):
     if app.calibredb_wrap.remove_book(book_id) == 0:
         flash_success('Book #%i deleted successfully!' % book_id)
@@ -224,7 +229,7 @@ def delete_book(book_id):
         flash_error('Could not delete book #%i' % book_id)
     return redirect(url_for("index"))
 
-@app.route('/book/<int:book_id>/formats/add', methods=['POST'])
+@app.route('/books/<int:book_id>/formats/add', methods=['POST'])
 def add_format(book_id):
     if 'format_upload' in request.files:
         for format_file in request.files.getlist('format_upload'):
@@ -245,7 +250,7 @@ def add_format(book_id):
         flash_error('Please select a file to upload')
         return redirect(url_for("book_edit", book_id=book_id))
 
-@app.route('/upload', methods=['POST'])
+@app.route('/books/upload', methods=['POST'])
 def upload():
     if 'books_upload' in request.files:
         for book_file in request.files.getlist('books_upload'):
@@ -266,10 +271,27 @@ def upload():
         flash_error('Please select a file to upload')
         return redirect(url_for("index"))
 
-@app.route('/book/<int:book_id>/convert', methods=['POST'])
+@app.route('/books/<int:book_id>/convert', methods=['POST'])
 def convert_book(book_id):
     if 'format_from' in request.form and 'format_to' in request.form:
         app.calibredb_wrap.convert_book(book_id, request.form['format_from'], request.form['format_to'])
         flash_success('Conversion started for book id %i from %s to %s' % (book_id, request.form['format_from'], request.form['format_to']))
         return redirect(url_for("list_tasks"))
     return redirect(url_for('index'))
+
+@app.route('/books/<int:book_id>/cover')
+def get_cover(book_id):
+    book = app.calibredb_wrap.get_book(book_id)
+    if book and book.has_cover:
+        return send_from_directory(os.path.join(
+            app.config['CALIBRE_LIBRARY_PATH'], book.path),
+            'cover.jpg')
+    return redirect('/static/img/default_cover.jpg')
+
+@app.route('/books/<int:book_id>/file/<book_format>/')
+def download_book_file(book_id, book_format):
+    fpath, fname = app.calibredb_wrap.get_book_file(book_id,
+            book_format.upper())
+    return send_from_directory(fpath, fname, conditional=True, attachment_filename=fname, as_attachment=True)
+
+
