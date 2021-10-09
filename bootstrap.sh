01f574d0394aa4657f878bc0cf85763b157622b2
Maxence <max@23.tf>
2021-10-09 02:36:44 +0000

    Update to latest jquery

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 # Download required web frontend libraries
 mkdir calibre_webui/static/css/ && mkdir calibre_webui/static/js/
-wget -q https://code.jquery.com/jquery-3.4.1.min.js -O calibre_webui/static/js/jquery.min.js
+wget -q https://code.jquery.com/jquery-3.6.0.min.js -O calibre_webui/static/js/jquery.min.js
 wget -q https://github.com/twbs/bootstrap/releases/download/v4.6.0/bootstrap-4.6.0-dist.zip -O bootstrap.zip
 unzip -jo bootstrap.zip *.css *.css.map -d calibre_webui/static/css/
 unzip -jo bootstrap.zip *.js *.js.map -d calibre_webui/static/js/
