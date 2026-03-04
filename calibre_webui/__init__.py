94c2428de019df0a80aec4a03ddb36c511849335
Maxence <max@23.tf>
2026-03-03 22:01:59 -0500

    support loading conf from environment

--- calibre_webui/__init__.py
+++ calibre_webui/__init__.py
@@ -13,6 +13,14 @@ app.config.from_object('calibre_webui.default_config')
 
 if os.path.isfile('/etc/calibre_webui/calibre_webui.cfg'):
     app.config.from_pyfile('/etc/calibre_webui/calibre_webui.cfg', silent=True)
+
+import calibre_webui.default_config as _default_config
+for _key in dir(_default_config):
+    if not _key.startswith('_') and _key in os.environ:
+        app.config[_key] = os.environ[_key]
+
+if not app.config.get('APP_SECRET_KEY'):
+    raise RuntimeError('APP_SECRET_KEY is not set')
 app.secret_key = app.config['APP_SECRET_KEY']
 app.FLASH = {'error': 'danger',
         'warning': 'warning',
